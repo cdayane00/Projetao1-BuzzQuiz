57478e7b7b74849f52093250e99114c35ca6dec3
Carla <carla.dayane.berredo@gmail.com>
2022-04-20 21:31:14 -0400

    verificacao

--- scripts.js
+++ scripts.js
@@ -8,23 +8,28 @@ function criacaoQuizz1(){
     conteudo.innerHTML += `<div class="titulo">
                                 <p>Comece pelo começo</p>
                             </div>
-                            <div class="criacaoBasica">
-                                <div class="organizaMsg">
-                                    <input type="text" placeholder="Título do seu quizz">
-                                    <input type="url" placeholder="URL da imagem do seu quizz">
-                                    <input type="text" placeholder="Quantidade de perguntas do quizz">
-                                    <input type="text" placeholder="Quantidade de níveis do quizz">
+                            <div class="criacaoBasica3">
+                                <div class="organizaMsg3">
+                                    <input id="1" type="text" placeholder="Título do seu quizz">
+                                    <input id="2" type="url"  placeholder="URL da imagem do seu quizz">
+                                    <input id="3" type="text" placeholder="Quantidade de perguntas do quizz">
+                                    <input id="4" type="text" placeholder="Quantidade de níveis do quizz">
                                 </div>
                             </div>
-                            <div onclick="verifica()" class="botao">
-                                <p class="textoBotao">Prosseguir para criar perguntas</p>
+                            <div onclick="verifica()" class="botao3">
+                                <p class="textoBotao3">Prosseguir para criar perguntas</p>
                             </div>`
+    
 }
 function verifica(){
-    let titulo = document.getElementById("1").value;
-    let url = document.getElementById("2").value;
-    if(titulo.length < 20 || titulo.length > 65){
-        alert("Preencha os dados corretamente titulo");
+    const titulo = document.getElementById("1").value;
+    const url = document.getElementById("2").value;
+    const perguntas = document.getElementById("3").value;
+    const nivel = document.getElementById("4").value;
+    //const verificaURL = url.exec(document.getElementById("url").value);
+    //alert(verificaURL)
+    if(titulo.length < 20 || titulo.length > 65 || perguntas < 3 || nivel < 2){
+         alert("Preencha os dados corretamente");
     }
 
 }
